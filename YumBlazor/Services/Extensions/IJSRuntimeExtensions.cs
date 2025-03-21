e671c2927224a6d2e2e2d8e78b2b3330083fe7f6
KLRambo <KevinRambo@hotmail.com>
2025-03-21 17:35:25 -0400

    Added Toastr functioanlity for category

---
+++ YumBlazor/Services/Extensions/IJSRuntimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.JSInterop;
+
+namespace YumBlazor.Services.Extensions
+{
+    public static class IJSRuntimeExtensions
+    {
+        public static async Task ToastrSuccess(this IJSRuntime Ijsruntime, string message)
+        {
+            await Ijsruntime.InvokeVoidAsync("ShowToastr", "success", message);
+        }
+        public static async Task ToastrError(this IJSRuntime Ijsruntime, string message)
+        {
+            await Ijsruntime.InvokeVoidAsync("ShowToastr", "error", message);
+        }
+
+    }
+}
